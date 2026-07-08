be585d5011f1b4304662961c5eeaf158e82ad06a
Mathew Han <mathewjhan@gmail.com>
2026-07-08 02:01:10 -0700

    [misc] update example

--- examples/multi_lora/single_run_disagg.sh
+++ examples/multi_lora/single_run_disagg.sh
@@ -39,9 +39,9 @@ ray job submit --address="http://127.0.0.1:8265" \
    --multi-lora-adapter "dapo_math" "examples/multi_lora/adapters/dapo_math.yaml" \
    --multi-lora-adapter "gsm8k" "examples/multi_lora/adapters/gsm8k.yaml" \
    --multi-lora-disable-service-mode \
-   --sglang-lora-backend triton \
    --pause-generation-mode in_place \
-   --max-weight-staleness 2 \
+   --max-weight-staleness 3 \
+   --use-tis \
    \
    --apply-chat-template \
    --rollout-shuffle \
